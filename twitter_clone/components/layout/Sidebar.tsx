94e3f535a3ba78a309c2a1d5edbff22170ceecb9
henicodes <henicodes@gmail.com>
2023-04-18 21:29:22 +0300

    created sidebars layouts

--- twitter_clone/components/layout/Sidebar.tsx
+++ twitter_clone/components/layout/Sidebar.tsx
@@ -1,18 +1,22 @@
 import { BsHouseFill, BsBellFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
 import SidebarLogo from './SidebarLogo';
+import SidebarItem from './SidebarItem';
 const Sidebar = () => {
     const items = [
+        /* Home sidebar*/ 
         {
             label: 'Home',
             href: '/',
             icon: BsHouseFill
         },
+        /* Notifications sidebar*/ 
         {
             label: 'Notifications',
             href: '/notifications',
             icon: BsBellFill
         },
+        /* profile sidebar*/ 
         {
             label: 'Profile',
             href: '/user/123',
@@ -24,6 +28,14 @@ const Sidebar = () => {
           <div className="flex flex-col items-end">
             <div className="space-y-2 lg:w-[230-px]">
 <SidebarLogo/>
+{items.map((item) => ( <SidebarItem 
+        key={item.href}
+        href={item.href}
+        label={item.label}
+        icon={item.icon}
+        />
+    ))
+}
         </div>
     </div> 
 </div>
